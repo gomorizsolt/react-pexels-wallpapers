6b53f4ad0fb3b1309721b2025e0a6456af30f8a1
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-06-16 15:11:43 +0200

    Remove getApiKey(), fix environment variable referencing.

--- src/axios.ts
+++ src/axios.ts
@@ -1,16 +1,8 @@
 import axios from "axios";
 
-function getApiKey() {
-    if (process.env.NODE_ENV === "production") {
-        return process.env.PEXELS_API_KEY;
-    }
-
-    return process.env.REACT_APP_PEXELS_API_KEY;
-}
-
 const instance = axios.create({
     baseURL: "https://api.pexels.com/v1",
-    headers: { Authorization: getApiKey() },
+    headers: { Authorization: process.env.REACT_APP_PEXELS_API_KEY },
 });
 
 export default instance;
